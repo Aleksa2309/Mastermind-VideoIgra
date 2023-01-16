8e6519fb020db935e9d314129ff7d0ad05584d62
Aleksa2309 <aleksajag@gmail.com>
2023-01-16 22:01:01 +0100

    Pokusaj pushovanja opet

--- Mastermind-VideoIgra/Form1.cs
+++ Mastermind-VideoIgra/Form1.cs
@@ -10,6 +10,7 @@ using System.Windows.Forms;
 
 namespace Mastermind_VideoIgra
 {
+   
     public partial class Form1 : Form
     {
         public Form1()
@@ -19,8 +20,12 @@ namespace Mastermind_VideoIgra
 
         private void button1_Click(object sender, EventArgs e)
         {
+            Form1 form1 = new Form1();
             Form2 form2 = new Form2();
+            
             form2.Show();
+            form1.Close();
+           
         }
     }
 }
